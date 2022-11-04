749f3b54e68d122fe84670618009f5ab0ecbed07
muhammed mubarak <oyindamola850@gmail.com>
2022-11-04 22:32:38 +0100

    model

--- controllers/blog.ts
+++ controllers/blog.ts
@@ -1,19 +1,72 @@
 import express, { Request, Response } from "express";
+import Blog from "../model/blogModel";
 
-const getBlogs = (req: Request, res: Response) => {
-  res.status(200).json({ message: "I am a get request" });
+const getBlogs = async (req: Request, res: Response) => {
+  const blogs = await Blog.find();
+  res.status(200).json({ blogs: blogs });
 };
 
-const createBlog = (req: Request, res: Response) => {
-  res.status(200).json({ message: "I am a post request" });
+const createBlog = async (req: Request, res: Response) => {
+  if (!req.body.title) {
+    res.status(400).json({ message: "Please add a title" });
+    return;
+  }
+  if (!req.body.description) {
+    res.status(400).json({ message: "Please add a description" });
+    return;
+  }
+
+  try {
+    const newBlog = await Blog.create({
+      title: req.body.title,
+      description: req.body.description,
+    });
+
+    res.status(200).json({
+      message: "New post created",
+      newBlog,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error,
+    });
+  }
 };
 
-const updateBlog = (req: Request, res: Response) => {
-  res.status(200).json({ message: "I am a update request" });
+const updateBlog = async (req: Request, res: Response) => {
+  const blogID = await Blog.findById(req.params.id);
+
+  if (!blogID) {
+    res.status(400).json({ message: "Post not found!!!" });
+  }
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json({ message: "Blog updated successfully", updatedBlog });
+  } catch (error) {
+    res.status(400).json({
+      message: error,
+    });
+  }
 };
 
-const deleteBlog = (req: Request, res: Response) => {
-  res.status(200).json({ message: "I am a delete request" });
+const deleteBlog = async (req: Request, res: Response) => {
+  const blogID = await Blog.findById(req.params.id);
+
+  if (!blogID) {
+    res.status(400).json({ message: "Post not found!!!" });
+  }
+
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "Blog deleted successfully" });
+  } catch (error) {
+    res.status(400).json({
+      message: error,
+    });
+  }
 };
 
 export { getBlogs, createBlog, updateBlog, deleteBlog };
