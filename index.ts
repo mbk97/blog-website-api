54b6f2be79f97afed80511b7446a6a3ee46e4c70
muhammed mubarak <oyindamola850@gmail.com>
2022-11-04 21:56:50 +0100

    mongo connection

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import { blogRouter } from "./routes/blogRoutes";
+import { connectDB } from "./config/db";
 
 dotenv.config();
+connectDB();
+
 const app: Express = express();
 const port = process.env.PORT || 5000;
 
